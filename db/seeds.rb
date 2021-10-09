5d0f7331b1973917f2c7b534677a64c9bb4d3304
Marcos Mourao <marcosfrimourao@gmail.com>
2021-10-09 16:43:04 -0300

    index and show working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  new_article = Article.new(title: [Faker::Company.name, Faker::Company.industry].join(' - '),
+                            content: Faker::Lorem.paragraphs)
+  new_article.save
+end
